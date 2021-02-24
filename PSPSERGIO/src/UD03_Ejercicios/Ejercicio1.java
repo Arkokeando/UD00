ce2c52b2a66f38c8edb5869c8d648c182423de9a
sergi <sergi@DESKTOP-RR5PN5E>
2021-02-24 16:27:38 +0100

    Las pruebas

---
+++ PSPSERGIO/src/UD03_Ejercicios/Ejercicio1.java
@@ -0,0 +1,46 @@
+package UD03_Ejercicios;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class Ejercicio1 {
+	
+	public static void main(String[] args) {
+		URL url = null;
+		URLConnection conexion = null;
+
+		try {
+			url = new URL("https://psp2021site.000webhostapp.com/suma_parametros.php");
+			conexion = url.openConnection();
+			conexion.setDoOutput(true);
+			// Le pasamos los parámetros por cadena
+			String cadena = "PARAM_A=5&PARAM_B=6";
+			PrintWriter output = new PrintWriter(conexion.getOutputStream());
+			output.write(cadena);
+			output.close();
+			BufferedReader bReader;
+			InputStream inputStream = conexion.getInputStream();
+			bReader = new BufferedReader(new InputStreamReader(inputStream));
+			String inputLine;
+			while ((inputLine = bReader.readLine()) != null) {
+				// Borramos todas las marcas de las etiquetas HTML con el replaceAll
+
+				System.out.println(inputLine.replaceAll("\\<[^>]*>", ""));
+			}
+			bReader.close();
+
+
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ PSPSERGIO/src/UD03_Ejercicios_2/Principal.java
@@ -0,0 +1,13 @@
+package UD03_Ejercicios_2;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		ManagerCalculadora mc = new ManagerCalculadora();
+		mc.sumar(2, 3);
+		mc.restar(2, 3);
+		mc.multiplicar(2, 3);
+
+	}
+
+}
